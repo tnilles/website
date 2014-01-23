e2b99d8938a7cd64efc9b8a59eaeb56d48fc6911
Thierry Nilles <eagle1792@gmail.com>
2014-01-23 05:34:32 +0100

    Fix the header at 470px

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,39 @@
 'use strict';
 
-var golElm = $('#game-of-life');
-golElm.parent().height(window.innerHeight);
-var headerGol = new Gol(golElm.get(0), golElm.parent().width(), golElm.parent().height());
-headerGol.addCellsOnHover();
+(function() {
+    var golElm, headerGol, headerHeight, headerMinHeight, blockSize = 8;
 
-window.addEventListener('resize', function() {
-    if (headerGol) {
-        headerGol.resize(golElm.parent().width(), golElm.parent().height());
-    }
-});
+    golElm = $('#game-of-life');
+
+    headerHeight = 470;
+
+/*    headerMinHeight = parseInt(golElm.parent().css('min-height'), 10);
+    if (headerMinHeight > $(window).height()) {
+        headerHeight = headerMinHeight;
+    } else {
+        headerHeight = $(window).height();
+    }*/
+
+    golElm.parent().height(headerHeight);
+
+    headerGol = new Gol({
+        elm: golElm.get(0),
+        blockSize: blockSize,
+        width: $(window).width(),
+        height: headerHeight
+    });
+    headerGol.addCellsOnHover();
+
+    window.addEventListener('resize', function() {
+        if (headerGol) {
+            /*if (headerMinHeight > $(window).height()) {
+                headerHeight = headerMinHeight;
+            } else {
+                headerHeight = $(window).height();
+            }*/
+
+            golElm.parent().height(headerHeight);
+            headerGol.resize($(window).width(), headerHeight);
+        }
+    });
+}());
