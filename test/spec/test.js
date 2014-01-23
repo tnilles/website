c59f8572ffb17177f172170edfdaffb7e49ce2c8
Thierry Nilles <eagle1792@gmail.com>
2014-01-23 05:35:18 +0100

    Add some tests for the game of life

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,10 +3,75 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    describe('Game of life', function() {
+        describe('Testing known figures', function() {
+            it('empty should remain empty', function() {
+                var blockSize = 3;
+                var golElm = $('#gol');
+                var gol = new Gol({
+                    elm: golElm.get(0),
+                    blockSize: blockSize,
+                    width: blockSize * 3,
+                    height: blockSize * 3
+                }); // 3x3 grid
 
+                var myGrid = [[0, 0, 0],
+                              [0, 0, 0],
+                              [0, 0, 0]];
+
+                gol.setAnimation(false);
+                gol.initGrid(myGrid);
+                gol.nextStep();
+
+                expect(gol.getGridContent()).to.deep.equal(myGrid);
+            });
+
+            it('square should remain square', function() {
+                var blockSize = 3;
+                var golElm = $('#gol');
+                var gol = new Gol({
+                    elm: golElm.get(0),
+                    blockSize: blockSize,
+                    width: blockSize * 3,
+                    height: blockSize * 3
+                }); // 3x3 grid
+
+                var myGrid = [[0, 0, 0],
+                              [0, 1, 1],
+                              [0, 1, 1]];
+
+                gol.setAnimation(false);
+                gol.initGrid(myGrid);
+                gol.nextStep();
+
+                console.log(gol.getGridContent(), myGrid);
+                expect(gol.getGridContent()).to.deep.equal(myGrid);
+            });
+
+            it('toad should iterate once', function() {
+                var blockSize = 3;
+                var golElm = $('#gol');
+                var gol = new Gol({
+                    elm: golElm.get(0),
+                    blockSize: blockSize,
+                    width: blockSize * 4,
+                    height: blockSize * 4
+                }); // 4x4 grid
+
+                var myGrid = [[0, 0, 0, 0],
+                              [0, 1, 1, 1],
+                              [1, 1, 1, 0],
+                              [0, 0, 0, 0]];
+
+                gol.setAnimation(false);
+                gol.initGrid(myGrid);
+                gol.nextStep();
+
+                console.log(gol.getGridContent(), myGrid);
+                expect(gol.getGridContent()).to.deep.equal([[0, 0, 1, 0],
+                                                            [1, 0, 0, 1],
+                                                            [1, 0, 0, 1],
+                                                            [0, 1, 0, 0]]);
             });
         });
     });
