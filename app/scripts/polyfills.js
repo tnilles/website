962a4087ece296ea481fe75004aa0c5f490ceacc
Thierry Nilles <eagle1792@gmail.com>
2014-01-25 02:21:04 +0100

    Add polyfills for Safari

---
+++ app/scripts/polyfills.js
@@ -0,0 +1,29 @@
+window.requestAnimationFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function(/* function */ callback, /* DOMElement */ element){
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
+(function(){
+    // prepare base perf object
+    if (typeof window.performance === 'undefined') {
+        window.performance = {};
+    }
+
+    if (!window.performance.now){
+        var nowOffset = Date.now();
+
+        if (performance.timing && performance.timing.navigationStart){
+            nowOffset = performance.timing.navigationStart
+        }
+
+        window.performance.now = function now(){
+            return Date.now() - nowOffset;
+        }
+    }
+})();
